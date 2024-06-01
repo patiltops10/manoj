f400ffd7274c842b46286b512ff844aee5f71b27
patiltops10 <patm3439@gmail.com>
2024-06-01 10:37:12 +0530

    add

---
+++ Module 3/basic logc prg/1.c
@@ -0,0 +1,11 @@
+// 1. Display This Information using printf 
+// a. Your Name 
+// b. Your Birth date 
+// c. Your Age 
+// d. Your Address 
+
+printf ("Name :Manoj Patil\n");
+printf ("DOB :August30, 1989\n");
+printf ("Age : 34\n");
+printf ("Address :Aaspas\n");
+
